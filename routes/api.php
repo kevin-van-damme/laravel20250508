d04a4b209c3eb65f73bad22fbfe0fc053bae738d
Kevin Van Damme <kevin.van.damme.pxl@protonmail.com>
2025-05-13 10:19:00 +0200

    changes in api.php

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('products', App\Http\Controllers\ProductController::class);
+Route::apiResource('categories', App\Http\Controllers\CategoryController::class);
